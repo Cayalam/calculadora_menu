50f3d7efa30731cc5bba6d295a601004ac9718a9
JdavidN <javier2214114@correo.uis.edu.co>
2022-09-10 08:02:36 -0500

    commit de el index

---
+++ calculadora_menu.py
@@ -0,0 +1,20 @@
+# Calculadora con menu
+
+from math import log
+
+print("---------------------------")
+print("----Calculadora-menu--------")
+
+# input
+bandera=False
+x=float(input("Dame el valor del numero x: "))
+y=float(input("Dame el valor del numero y: "))
+
+print("\nDame la opcion que deseas realizar:\n")
+#Se despliega el menu para selepcionar la opcion que deseas realizar:
+print("1.sumar(el primero mas el segundo)")
+print("2.restar(el primero menos el segundo)")
+print("3.multiplicar(el primero por el segundo)")
+print("4.dividir(el primero entre el segundo)")
+print("5.potencia(el primero a la potencia de el segundo)")
+print("6. logaritmo(logaritmo del primero por el segundo)")
